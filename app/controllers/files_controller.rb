44ca1600cce2150a1abe0e3f19338060c85303d3
Anthony Schmieder <AnthonySchmieder@nanometrics.ca>
2018-03-20 18:36:48 -0400

    add zipfile served at /files.zip

---
+++ app/controllers/files_controller.rb
@@ -0,0 +1,11 @@
+class FilesController < ApplicationController
+  def index
+    respond_to do |format|
+      format.zip { send_zip }
+    end
+  end
+
+  def send_zip
+    send_file(Rails.root.join("test_file.zip"), type: "application/zip", disposition: "attachment", filename: "download.zip")
+  end
+end
